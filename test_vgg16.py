525f691e359834e0755321fa31963023fd1c016a
dhatrija6753 <dhatrisukasi@gmail.com>
2025-12-05 02:41:48 -0500

    test_vgg16.py for model inference and submission CSV generation

--- test_vgg16.py
+++ test_vgg16.py
@@ -1,50 +1,49 @@
 import os
-import pandas as pd
 import torch
 from torch.utils.data import DataLoader
-from torchvision import models, transforms
-from dataset import HARCsvDataset
-from tqdm import tqdm
+import torchvision.transforms as T
+from dataset import HARDataset
+from torchvision.models import vgg16, VGG16_Weights
+import pandas as pd
 
-# Paths
-BASE_PATH = "/home/ubuntu/.cache/kagglehub/datasets/meetnagadia/human-action-recognition-har-dataset/versions/1/Human Action Recognition"
-TEST_CSV = os.path.join(BASE_PATH, "Testing_set.csv")
-TEST_IMG_DIR = os.path.join(BASE_PATH, "test")
+# ---------------- Paths ----------------
+BASE_DIR = '/home/ubuntu/.cache/kagglehub/datasets/meetnagadia/human-action-recognition-har-dataset/versions/1/Human Action Recognition'
+IMG_DIR = os.path.join(BASE_DIR, 'train')  # change to 'test' if test images are separate
+TEST_CSV = os.path.join(BASE_DIR, 'test_split.csv')
 
-# Transform
-transform_test = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406],
-                         [0.229, 0.224, 0.225])
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# ---------------- Transforms ----------------
+test_transforms = T.Compose([
+    T.Resize((224, 224)),
+    T.ToTensor(),
 ])
 
-# Dataset and Loader
-test_dataset = HARCsvDataset(TEST_CSV, TEST_IMG_DIR, transform=transform_test, has_labels=False)
+# ---------------- Dataset and Loader ----------------
+test_dataset = HARDataset(TEST_CSV, IMG_DIR, transform=test_transforms)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
 
-# Model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-num_classes = 15
-model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
-model.classifier[6] = torch.nn.Linear(4096, num_classes)
-model.load_state_dict(torch.load("best_vgg16.pth", map_location=device))
-model = model.to(device)
+# ---------------- Model ----------------
+model = vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
+num_features = model.classifier[6].in_features
+model.classifier[6] = torch.nn.Linear(num_features, len(test_dataset.labels))
+model.load_state_dict(torch.load('best_vgg16.pth', map_location=DEVICE))
+model = model.to(DEVICE)
 model.eval()
 
-# Predict
+# ---------------- Inference ----------------
 all_preds = []
 with torch.no_grad():
-    for imgs, img_names in tqdm(test_loader, desc="Testing"):
-        imgs = imgs.to(device)
+    for imgs, _ in test_loader:
+        imgs = imgs.to(DEVICE)
         outputs = model(imgs)
-        preds = outputs.argmax(dim=1)
+        preds = outputs.argmax(1)
         all_preds.extend(preds.cpu().numpy())
 
-# Save submission
+# ---------------- Save Predictions ----------------
 submission = pd.DataFrame({
-    "filename": [name for _, name in test_dataset],
-    "label": [HARCsvDataset.index_to_label(idx) for idx in all_preds]
+    'filename': test_dataset.df['filename'],
+    'pred_label': [test_dataset.labels[i] for i in all_preds]
 })
-submission.to_csv("submission.csv", index=False)
-print("Test predictions saved to submission.csv")
+submission.to_csv('submission.csv', index=False)
+print("Predictions saved to submission.csv")
